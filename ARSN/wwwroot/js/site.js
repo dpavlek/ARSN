7c83980c14aaa32f3c60e1b19e3cfbdbaea85755
VladimirAnic <vladimiranic@gmail.com>
2018-01-27 00:32:17 +0100

    Design and frontend changed

--- ARSN/wwwroot/js/site.js
+++ ARSN/wwwroot/js/site.js
@@ -9,7 +9,7 @@
             document.getElementById('remove_hidden_manual').classList.remove("hidden");
             document.getElementById('show_button_manual').classList.add("hidden"); 
             document.getElementById('remove_hidden_automatic').classList.add("hidden");
-            document.getElementById('show_button_automatic').classList.remove("hidden");
+            document.getElementById('show_button_automatic').classList.add("hidden");
         } 
         //Automatic adding for competitions remove hidden
         var buttonAutomaticAdd = document.getElementById('show_button_automatic')
@@ -19,7 +19,7 @@
             document.getElementById('remove_hidden_automatic').classList.remove("hidden");
             document.getElementById('show_button_automatic').classList.add("hidden");  
             document.getElementById('remove_hidden_manual').classList.add("hidden");
-            document.getElementById('show_button_manual').classList.remove("hidden");
+            document.getElementById('show_button_manual').classList.add("hidden");
         } 
 
         var ButtonAddToCompetitionManual = document.getElementById('list_it_manual')
